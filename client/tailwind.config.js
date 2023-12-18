a8cd5cad94b94e5a5f7159b0031015b807983a76
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-12-17 22:55:36 -0800

    project initialization complete. creating layout branch.

---
+++ client/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      hyperlink: "#88c3ea",
+      "page-gray": "#C6C6C6",
+      "text-color": "#FFFFFF",
+      "page-blue": "#0649AC",
+      black: "#000000",
+      white: "#FFFFFF",
+      primary: "#91897f",
+      info: "#493f46",
+      success: "#61a45e",
+      danger: "#f44336",
+      warning: "#de9426",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
