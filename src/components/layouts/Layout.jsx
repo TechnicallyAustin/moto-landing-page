725cdbed675e11b887592cbded6b6d286ab84681
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-12-22 13:01:55 -0800

    adjusting Header object

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -8,28 +8,24 @@ export default function Layout({ children }) {
   const headerStyles = {
     about: {
       title: "About",
-      description:
-        "Mobile Motorcycle services dedicated to keeping Motorcyclists on their bikes and out of the Shop.",
+      description: "Mobile Motorcycle services dedicated to keeping Motorcyclists on their bikes and out of the Shop.",
     },
     services: {
       title: "Services",
-      description:
-        "Keeping the rubber down can be more difficult than it needs to be. Here are a few ways we can help.",
+      description: "Keeping the rubber down can be more difficult than it needs to be. Here are a few ways we can help.",
     },
     team: {
       title: "Team",
-      description:
-        "Experts coming together from different fields, focused on improving motorcycling reliability and daily quality of life for fellow riders.",
+      description: "Experts coming together from different fields, focused on improving motorcycling reliability and daily quality of life for fellow riders.",
     },
     contact: {
       title: "Contact",
-      description:
-        "Have an idea? Like our idea or want to collaborate? Just send us a message and let's talk about it.",
+      description: "Have an idea? Like our idea or want to collaborate? Just send us a message and let's talk about it.",
     },
   };
 
-  const { title: headerTitle, description: headerDescription } =
-    headerStyles[title.toLowerCase()] || {};
+  const key =  headerStyles[title.toLowerCase()] || {};
+  const description = key["description"]
 
   return (
     <section
@@ -38,11 +34,11 @@ export default function Layout({ children }) {
     >
       <header className="w-full h-auto flex flex-col gap-3 justify-start items-center bg-white relative z-40">
         <h2 className="relative z-40 w-full h-auto text-3xl flex items-center">
-          {headerTitle}
+          {title}
         </h2>
         <div className="w-full h-1 bg-danger"></div>
         <h3 className="relative z-40 w-full h-auto text-md shadow-xl p-3 bg-danger text-off-white rounded-lg shadow-md">
-          {headerDescription}
+          {description}
         </h3>
       </header>
       {children}
